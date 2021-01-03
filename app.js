eb87887645867606dba754471561053787fd1791
Juan Pablo Mascaretti <juanpmascaretti@gmail.com>
2021-01-03 15:33:36 -0500

    not running

--- app.js
+++ app.js
@@ -11,10 +11,14 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.listen(3100);
+app.use(errorController.get404);
+
+app.listen(3000);
